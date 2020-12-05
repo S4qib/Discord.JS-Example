d01745add1fe1e19a23f83baffb4e7e898d739e5
S4qib <73705449+S4qib@users.noreply.github.com>
2020-12-05 12:36:37 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+const start = {
+    token: 'Your-Bot-Token-Here'
+};
+
+client.once('ready', () => {
+	console.log('--------------');
+  console.log('Discord.JS Bot Example')
+  console.log('Coded with <3 by Saqib')
+  console.log('--------------');
+});
+
+client.on('message', msg => {
+  if (msg.content === '!ping') {
+    msg.reply(`🏓\`${Date.now() - message.createdTimestamp}\`ms`);
+  }
+});
+
+client.login(config.token);
